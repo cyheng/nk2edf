491cd665ec2063949aed382378505de92e5239fe
wing <wing.chen@bioserenity.cn>
2024-12-13 09:34:09 +0800

    support for windows
    add test data

--- build.sh
+++ build.sh
@@ -1,4 +1,14 @@
 #!/bin/sh
 
-gcc main.c -Wall -Wextra -Wformat-nonliteral -Wformat-security -Wtype-limits -Wfatal-errors -O2 -s -D_LARGEFILE64_SOURCE -D_LARGEFILE_SOURCE -o nk2edf
-
+x86_64-w64-mingw32-gcc main.c \
+    -Wall \
+    -Wextra \
+    -Wformat-nonliteral \
+    -Wformat-security \
+    -Wtype-limits \
+    -Wfatal-errors \
+    -O2 \
+    -s \
+    -D_LARGEFILE64_SOURCE \
+    -D_LARGEFILE_SOURCE \
+    -o nk2edf.exe
